aa2e268459f1380e997899fd6d134a3e532f1296
Rick Sanchez <matinbeigi99@gmail.com>
2025-06-23 12:35:01 +0330

    up

--- src/common-header-footer.js
+++ src/common-header-footer.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
       </button>
       <nav id="main-nav" class="flex-col gap-2 text-base w-full sm:w-auto sm:flex-row sm:gap-6 sm:static sm:bg-transparent sm:rounded-none sm:shadow-none sm:flex hidden items-center justify-end mt-2 sm:mt-0 bg-blue-900/95 rounded-lg shadow-lg p-4 transition-all duration-200 ease-in-out z-20 sm:justify-end order-3">
-        <a href="main.html">پروژه‌ها</a>
+        <a href="projects.html">پروژه‌ها</a>
         <a href="ir.html">منابع ایرانی</a>
         <a href="Residences.html">اقامتگاه‌ها</a>
         <a href="SOS.html">شماره اضطراری</a>
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // نمایش منو به صورت flex روی دسکتاپ
         function handleResize() {
           if(window.innerWidth >= 640) {
-            nav.classList.remove('hidden');
+            nav.class
             nav.classList.add('flex');
           } else {
             nav.classList.add('hidden');
